6c7f14509e1b3330e8fd99c361196cd814a1be80
Matheus Oliveira <matheus.oliveira@tecnoval.com.br>
2022-09-28 20:34:09 -0300

    Atualizando...

--- src/components/Parceiro/index.js
+++ src/components/Parceiro/index.js
@@ -2,12 +2,49 @@ import circle from "../../assets/gif/circle.gif";
 import headphones from "../../assets/img/headphones.png";
 import efi from "../../assets/img/efi-partner-premiercoralis.jpg";
 import product from "../../assets/img/product.png";
-import G7expert from "../../assets/img/G7expert.png";
-import EFI from "../../assets/img/EFI.png";
-import CreativeEdge from "../../assets/img/CreativeEdge.png";
-import { NavLink } from "react-router-dom";
+
+import efi1 from "../../assets/img/efi1.png";
+import efi2 from "../../assets/img/efi2.png";
+import efi3 from "../../assets/img/efi3.png";
+
+import efi1_ from "../../assets/img/efi1_.jpeg"
+import efi2_ from "../../assets/img/efi2_.jpeg";
+import efi3_ from "../../assets/img/efi3_.jpeg";
+
+import Swal from 'sweetalert2';
+
 
 const Index = () => {
+    const Card1 = () => {
+        Swal.fire({
+            title: 'Fiery XF',
+            text: 'Introduzir o Fiery XF 7 em sua empresa de grande ou super-amplo é a maneira mais fácil de obter resultados de cores superiores, obter o máximo de produtividade e lucrar com os insights de negócios.',
+            imageUrl: efi1_,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+    }
+    const Card2 = () => {
+        Swal.fire({
+            title: 'JobFlow',
+            text: 'O Fiery JobFlow ™ automatiza as etapas de preparação do trabalho em fluxos de trabalho de pré-impressão para produzir arquivos prontos para impressão. Este software de automação de fluxo de trabalho de impressão fácil de usar aumenta a produtividade e reduz os custos.',
+            imageUrl: efi3_,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+    }
+    const Card3 = () => {
+        Swal.fire({
+            title: 'Prep-it',
+            text: 'O Fiery Prep-it ™ é uma solução de software poderosa e fácil de usar para a preparação, layout e produção automatizada de trabalhos de impressão para corte.',
+            imageUrl: efi2_,
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+          })
+    }
     return (
         <>
             <div className="w-full h-[230px] mt-[80px] bg-efi  z-40"></div>
@@ -28,15 +65,15 @@ const Index = () => {
                         <img className="w-full svg svg-elem-1 active" src={product} alt="Sunset in the mountains" />
                     </div>
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Best products</div>
+                        <div class="font-bold text-xl mb-2">Melhores produtos</div>
                         <p class="text-gray-700 text-base">
-                            EFI is leading the worldwide transformation from analog to digital imaging. Powering this transformation is our commitment to reinvesting 16% of our revenue into research and development every year. We don’t just see that as an investment in our company. It’s an investment in yours as well.
+                        A EFI está liderando a transformação mundial de imagens analógicas para digitais. Alimentar essa transformação é nosso compromisso de reinvestir 16% de nossa receita em pesquisa e desenvolvimento todos os anos. Não vemos isso apenas como um investimento em nossa empresa. É um investimento no seu também.
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#transformação</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#mundial</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#EFI</span>
                     </div>
                 </div>
 
@@ -46,15 +83,15 @@ const Index = () => {
                         <img className="w-full svg svg-elem-1 active" src={headphones} alt="Sunset in the mountains" />
                     </div>
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Best opportunity</div>
+                        <div class="font-bold text-xl mb-2"> Melhores oportunidades</div>
                         <p class="text-gray-700 text-base">
-                            From Fiery to superwide inkjet, from the lowest cost per label to the most automated business processes, EFI develops products to support a complete end-to-end workflow for customers worldwide – giving partners a distinct competitive advantage to succeed.
+                        Do Fiery ao jato de tinta superwide, do menor custo por etiqueta aos processos de negócios mais automatizados, a EFI desenvolve produtos para oferecer suporte a um fluxo de trabalho completo de ponta a ponta para clientes em todo o mundo – dando aos parceiros uma vantagem competitiva distinta para o sucesso.
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#automatizados</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tinta</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#EFI</span>
                     </div>
                 </div>
 
@@ -64,20 +101,20 @@ const Index = () => {
                         <img className="w-full svg svg-elem-1 active" src={headphones} alt="Sunset in the mountains" />
                     </div>
                     <div class="px-6 py-4">
-                        <div class="font-bold text-xl mb-2">Best support</div>
-                        <p class="text-gray-700 text-base">
-                            Our 3,000+ worldwide team members prove our commitment to you every day by developing breakthrough technologies which, in turn, can help accelerate your sales of EFI solutions now and into the future.
+                        <div class="font-bold text-xl mb-2">Melhor suporte</div>
+                        <p class="text-gray-700 text-base mb-12">
+                            Nossos mais de 3.000 membros de equipe em todo o mundo comprovam nosso compromisso com você todos os dias desenvolvendo tecnologias inovadoras que, por sua vez, podem ajudar a acelerar suas vendas de soluções EFI agora e no futuro.
                         </p>
                     </div>
                     <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#compromisso</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#equipe</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#EFI</span>
                     </div>
                 </div>
             </div>
-            <div className="w-full h-[680px] flex mt-20">
-                <div className="w-[55%]">
+            <div className="w-full flex mt-5">
+                <div className="w-[55%] py-[10%]">
                     <div className="text-[45px] pl-[93px] text-[#393C3E]">The <b className="font-medium">Premier Partner Program</b></div>
                     <div>
                         <div className="text-[18px] font-light pl-[93px] pr-[70px] mt-6 leading-[35px]">All EFI Fiery Display Graphics resellers in Latin America are required to have at least one person certified to act as Technical Sales Support and also to perform installations and training of new customers. Resellers must also provide first level support.</div>
@@ -86,20 +123,22 @@ const Index = () => {
                         <div className="text-[18px] font-light pl-[93px] pr-[70px] mt-6 leading-[35px]">EFI Fiery XF Software has more than 70,000 installations worldwide. Coralis is responsible, as Master Distributor for LATAM, for creating the color culture, production automation and resource savings using EFI Fiery Display Graphics technologies.</div>
                     </div>
                 </div>
-                <div className="w-[45%]">
-                    <img alt="Efi" src={efi} />
+                <div className="w-[45%] flex justify-center items-center">
+                    <div className="w-[80%]"><img alt="Efi" src={efi} /></div>
                 </div>
             </div>
-            <div className="w-full flex justify-center mt-10 text-[40px] font-light text-[#134780]">EFI <b className="font-semibold">Fiery Solutions</b></div>
-            <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+            <div className="w-full py-[5%]">
+                <div className="w-full flex justify-center text-[40px] font-light text-[#134780]">EFI <b className="font-semibold">Fiery Solutions</b></div>
+                <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+                    
                     {/* <!-- box-1 --> */}
                     <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
                         <div class="w-sm">
-                            <img class="w-64" src={EFI} alt="Logo EFI" />
+                            <img class="w-64" src={efi1} alt="Logo EFI" />
                             <div class="mt-4 text-[#134780] text-center">
-                                <h1 class="text-xl font-bold">EFI</h1>
-                                <p class="mt-4 text-gray-600 mb-10">Todos nós da EFI estamos incansavelmente concentrados em apenas...</p>
-                                <NavLink className="mt-8 mb-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200" to="/Efi">MAIS</NavLink>
+                                <h1 class="text-xl font-bold">Fiery XF</h1>
+                                <p class="mt-4 text-gray-600">Introduzir o Fiery XF 7 em sua empresa de grande ou super-amplo é a maneira mais fácil...</p>
+                                <button className="mt-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200" onClick={Card1}>MAIS</button>
                             </div>
                         </div>
                     </div>
@@ -107,11 +146,11 @@ const Index = () => {
                     {/* <!-- box-2 --> */}
                     <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
                         <div class="w-sm">
-                            <img class="w-64" src={G7expert} alt="Logo G7 Expert" />
+                            <img class="w-64" src={efi2} alt="Logo G7 Expert" />
                             <div class="mt-4 text-green-600 text-center">
-                                <h1 class="text-xl font-bold text-[#134780]">G7 Expert</h1>
-                                <p class="mt-4 text-gray-600">A metodologia de calibração G7 é a melhor, mais simples e eficiente forma...</p>
-                                <button class="mt-8 mb-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200">MAIS</button>
+                                <h1 class="text-xl font-bold text-[#134780]">JobFlow</h1>
+                                <p class="mt-4 text-gray-600">O Fiery JobFlow ™ automatiza as etapas de preparação do trabalho em fluxos...</p>
+                                <button class="mt-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200" onClick={Card2}>MAIS</button>
                             </div>
                         </div>
                     </div>
@@ -119,14 +158,15 @@ const Index = () => {
                     {/* <!-- box-3 --> */}
                     <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
                         <div class="w-sm">
-                            <img class="w-64" src={CreativeEdge} alt="Logo Creative Egde" />
+                            <img class="w-64" src={efi3} alt="Logo Creative Egde" />
                             <div class="mt-4 text-green-600 text-center">
-                                <h1 class="text-xl font-bold text-[#134780]">Creative Edge</h1>
-                                <p class="mt-4 text-gray-600">É a desenvolvedora de um conjunto premiado de aplicativos para embalagem...</p>
-                                <button class="mt-8 mb-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200">MAIS</button>
+                                <h1 class="text-xl font-bold text-[#134780]">Prep-it</h1>
+                                <p class="mt-4 text-gray-600">O Fiery Prep-it ™ é uma solução de software poderosa e fácil de usar para a preparação...</p>
+                                <button class="mt-4 mb-4 py-2 px-14 rounded-full bg-[#134780] text-white tracking-widest hover:bg-[#134880dc] transition duration-200" onClick={Card3}>MAIS</button>
                             </div>
                         </div>
                     </div>
+                </div>
             </div>
         </>
     )
