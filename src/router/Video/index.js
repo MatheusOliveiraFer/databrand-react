162fd2c583849f28598074c1f16c2e9542a1a4ee
Matheus Oliveira <matheus.oliveira@tecnoval.com.br>
2022-11-01 21:29:28 -0300

    Atualizando

--- src/router/Video/index.js
+++ src/router/Video/index.js
@@ -3,7 +3,7 @@ import video from "../../assets/video/databrand.mp4";
 const Index = () => {
     return (
         <div className="w-full h-screen">
-            <video src={video} autoPlay loop muted />
+            <video src={video} autoPlay={true} muted={true} loop={true} />
         </div>
     );
 }
